1a0168bdae7843295ccbaa5be1a81f9eb2e5ee57
IanBet1 <ianbet@outlook.com.br>
2017-10-25 23:43:00 -0200

    Criação de funções get e set e definição de atributos.

--- model/lojaComponente.php
+++ model/lojaComponente.php
@@ -1,8 +1,41 @@
 <?php
 	class lojaComponente {
+		private $logoLoja;
 		private $nomeLoja;
 		private $valorLoja;
 		private $linkLoja;
+
+		//Funções de logoLoja
+		public function getLogoLoja(){
+			return $this -> logoLoja;
+		}
+		public function setLogoLoja($logoLoja){
+			$this -> logoLoja = $logoLoja;
+		}
+
+		//Funções de nomeLoja
+		public function getNomeLoja(){
+			return $this -> nomeLoja;
+		}
+		public function setNomeLoja($nomeLoja){
+			$this -> nomeLoja = $nomeLoja;
+		}
+
+		//Funções de valorLoja
+		public function getValorLoja(){
+			return $this -> valorLoja;
+		}
+		public function setValorLoja($valorLoja){
+			$this -> valorLoja = $valorLoja;
+		}
+
+		//Funções de linkLoja
+		public function getLinkLoja(){
+			return $this -> linkLoja;
+		}
+		public function setLinkLoja($linkLoja){
+			$this -> linkLoja = $linkLoja;
+		}
 
 	}
 ?>
