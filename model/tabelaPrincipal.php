ec4f3561204c5f690261d3bb1d30e8d7e59da040
IanBet1 <ianbet@outlook.com.br>
2017-10-25 23:43:43 -0200

    Ajuste de funções get e set. De privadas para públicas.

--- model/tabelaPrincipal.php
+++ model/tabelaPrincipal.php
@@ -12,50 +12,50 @@
 		}
 
 		//Funções de componenteBasico
-		private function getComponenteBasico(){
+		public function getComponenteBasico(){
 			return $this  ->  componenteBasico;
 		}
-		private function setComponenteBasico($componenteBasico){
+		public function setComponenteBasico($componenteBasico){
 			$this -> componenteBasico = $componenteBasico;
 		}
 
 		//Funções de imgComponente
-		private function getImgComponente(){
+		public function getImgComponente(){
 			return $this -> imgComponente;
 		}
-		private function setComponenteBasico($imgComponente){
+		public function setComponenteBasico($imgComponente){
 			$this -> imgComponente = $imgComponente;
 		}
 
 		//Funções de descricaoComponente
-		private function getDescricaoComponente(){
+		public function getDescricaoComponente(){
 			return $this -> descricaoComponente;
 		}
-		private function setComponenteBasico($descricaoComponente){
+		public function setComponenteBasico($descricaoComponente){
 			$this -> descricaoComponente = $descricaoComponente;
 		}
 
 		//Funções de valorComponente
-		private function getValorComponente(){
+		public function getValorComponente(){
 			return $this -> valorComponente;
 		}
-		private function setComponenteBasico($valorComponente){
+		public function setComponenteBasico($valorComponente){
 			$this -> valorComponente = $valorComponente;
 		}
 
 		//Funções de nomeLoja
-		private function getNomeLoja(){
+		public function getNomeLoja(){
 			return $this -> nomeLoja;
 		}
-		private function setComponenteBasico($nomeLoja){
+		public function setComponenteBasico($nomeLoja){
 			$this -> nomeLoja = $nomeLoja;
 		}
 
 		//Funções de linkOferta
-		private function getLinkOferta(){
+		public function getLinkOferta(){
 			return $this -> linkOferta;
 		}
-		private function setComponenteBasico($linkOferta){
+		public function setComponenteBasico($linkOferta){
 			$this -> linkOferta = $linkOferta;
 		}
 
