f613521c3ba7846ab1026f21a0bf116607aa48fc
IanBet1 <ianbet@outlook.com.br>
2017-10-28 17:13:38 -0200

    Nova tabela de soquetes.

--- bd/scriptCreate.sql
+++ bd/scriptCreate.sql
@@ -8,6 +8,13 @@ CREATE TABLE IF NOT EXISTS categoriaComponente  (
   PRIMARY KEY (idComponente)
 );
 
+CREATE TABLE IF NOT EXISTS socketComponente  (
+  idSocket INT NOT NULL AUTO_INCREMENT,
+  marcaComponente VARCHAR(12) NOT NULL,
+  nomeSocket VARCHAR(12) NOT NULL,
+  PRIMARY KEY (idSocket)
+);
+
 INSERT INTO categoriaComponente (idComponente, idCategoria, nomeComponente)
 VALUES
   (null, 95, 'processador'),
@@ -17,3 +24,25 @@ VALUES
   (null, 35, 'placavideo'),
   (null, 9550, 'fonte'),
   (null, 114, 'gabinete');
+
+INSERT INTO socketComponente (idSocket, marcaComponente, nomeSocket)
+VALUES
+  (null, 'Intel', 'LGA 775'),
+  (null, 'Intel', 'LGA 1366'),
+  (null, 'Intel', 'LGA 1156'),
+  (null, 'Intel', 'LGA 1155'),
+  (null, 'Intel', 'LGA 2011'),
+  (null, 'Intel', 'LGA 1150'),
+  (null, 'Intel', 'LGA 1151'),
+  (null, 'Intel', 'LGA 2066'),
+  (null, 'AMD', 'F'),
+  (null, 'AMD', 'AM1'),
+  (null, 'AMD', 'AM2'),
+  (null, 'AMD', 'AM2+'),
+  (null, 'AMD', 'AM3'),
+  (null, 'AMD', 'AM3+'),
+  (null, 'AMD', 'FM1'),
+  (null, 'AMD', 'FM2'),
+  (null, 'AMD', 'FM2+'),
+  (null, 'AMD', 'AM4'),
+  (null, 'AMD', 'TR4');
