abf75290e960d6f6717954c9d27e074209efd0a6
IanBet1 <ianbet@outlook.com.br>
2017-11-11 04:25:56 -0200

    Criação de classe para seleção de componente e gravação na session.

--- controller/addCompSession.php
+++ controller/addCompSession.php
@@ -1,3 +1,29 @@
 <?php
-  echo "First test";
+  $componente = $_POST['componente'];
+  $idComponente = $_POST['idComponente'];
+
+  session_start();
+  switch($componente){
+    case 'processador':{
+      $_SESSION["processador"] = $idComponente;
+    };break;
+    case 'placamae':{
+      $_SESSION["placamae"] = $idComponente;
+    };break;
+    case 'memoriaram':{
+      $_SESSION["memoriaram"] = $idComponente;
+    };break;
+    case 'hd/ssd':{
+      $_SESSION["hd/ssd"] = $idComponente;
+    };break;
+    case 'placavideo':{
+      $_SESSION["placavideo"] = $idComponente;
+    };break;
+    case 'fonte':{
+      $_SESSION["fonte"] = $idComponente;
+    };break;
+    case 'gabinete':{
+      $_SESSION["gabinete"] = $idComponente;
+    };break;
+  }
 ?>
